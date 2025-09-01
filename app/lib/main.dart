5c01b235d26d6fa23ae2a024a8220c5d35d79c5e
Worachat <worachat.smn@gmail.com>
2025-09-01 23:51:19 +0700

    feat: implement navigatebar

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'app_routes.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
-      title: 'PhamaLink',
+      title: 'PharmaLink',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green,
       ),
     );
   }
