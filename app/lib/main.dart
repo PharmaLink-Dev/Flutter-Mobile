42d209a04dd0c1799dda3b96a4d8bb64365b616c
Weera Arsasutcharit <pukdezgm@gmail.com>
2025-11-21 01:51:34 +0700

    refactor: update ingredient import and registration for consistency

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:app/features/history/data/fda_scan.dart';
-import 'package:app/features/history/data/ingredient.dart';
+import 'package:app/features/history/data/history_ingredient.dart';
 import 'package:app/features/history/data/scan_history.dart';
 import 'package:app/service/supabase_init.dart';
 import 'package:flutter/material.dart';
@@ -11,15 +11,16 @@ Future<void> main() async {
   await SupabaseService.init();
 
   await Hive.initFlutter();
-  Hive.registerAdapter(IngredientAdapter()); // typeId: 1
+  Hive.registerAdapter(HistoryIngredientAdapter()); // typeId: 1
   Hive.registerAdapter(ScanHistoryAdapter()); // typeId: 0
   Hive.registerAdapter(FdaScanAdapter()); // typeId: 2
 
   // Open the boxes
-  await Hive.openBox<Ingredient>('ingredients'); // Good practice to have a box for them
+  await Hive.openBox<HistoryIngredient>(
+    'ingredients',
+  ); // Good practice to have a box for them
   await Hive.openBox<ScanHistory>('history');
   await Hive.openBox<FdaScan>('fda_scans');
-
 
   runApp(const MyApp());
 }
@@ -32,10 +33,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
       title: 'PharmaLink',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.green,
-      ),
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.green),
     );
   }
 }
